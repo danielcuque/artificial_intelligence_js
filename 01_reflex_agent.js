ae8553f2283b6396c1fcbb3865ca1ad869a42551
danielcuque <3024465830102@ingenieria.usac.edu.gt>
2024-08-05 15:10:56 -0600

    all states

--- 01_reflex_agent.js
+++ 01_reflex_agent.js
@@ -1,26 +1,81 @@
-function reflex_agent(location, state){
-   	if (state=="DIRTY") return "CLEAN";
-   	else if (location=="A") return "RIGHT";
-   	else if (location=="B") return "LEFT";
+const LEFT_POSITION = "A";
+const RIGHT_POSITION = "B";
+
+const DIRTY_STATE = "DIRTY";
+const CLEAN_STATE = "CLEAN";
+
+const LEFT_ACTION = "LEFT";
+const RIGHT_ACTION = "RIGHT";
+const CLEAN_ACTION = "CLEAN";
+const MESS_ACTION = "DIRTY";
+
+
+function getRandom() {
+    return Math.floor(Math.random() * 2);
 }
 
-function test(states){
-      	var location = states[0];		
-      	var state = states[0] == "A" ? states[1] : states[2];
-      	var action_result = reflex_agent(location, state);
-      	document.getElementById("log").innerHTML+="<br>Location: ".concat(location).concat(" | Action: ").concat(action_result);
-      	if (action_result == "CLEAN"){
-        	if (location == "A") states[1] = "CLEAN";
-         	else if (location == "B") states[2] = "CLEAN";
-      	}
-      	else if (action_result == "RIGHT") states[0] = "B";
-      	else if (action_result == "LEFT") states[0] = "A";	
-		
-	if (states[1] == "CLEAN"  && Math.random() < 0.5) states[1] = "DIRTY";
-	if (states[2] == "CLEAN"  && Math.random() < 0.5) states[2] = "DIRTY";
+function reflex_agent(location, state) {
+    if (state === DIRTY_STATE && getRandom() === 1) return CLEAN_ACTION;
+    if (state === CLEAN_STATE && getRandom() === 1) return MESS_ACTION;
 
-	setTimeout(function(){ test(states); }, 2000);
+    if (location === LEFT_POSITION) return RIGHT_ACTION;
+    if (location === RIGHT_POSITION) return LEFT_ACTION;
 }
 
-var states = ["A","DIRTY","DIRTY"];
+
+const traversedStates = []
+
+const addState = (newState) => {
+    const [robot, aState, BState] = newState;
+    const posibleStates = traversedStates.find(state => {
+        const [r, a, b] = state;
+        return a === aState && b === BState && robot === r;
+    })
+    if (!posibleStates) {
+        traversedStates.push([...newState]);
+        console.log({ traversedStates })
+    }
+}
+
+function test(states) {
+
+    if (traversedStates.length === 8) {
+        return document.getElementById("log").innerHTML += `
+        <br>Todos los estados han sido recorridos<br>
+		<ul>
+        ${
+			[...traversedStates].map(state => `
+				<li>${state[0]}:  ${state[1]} - ${state[2]}</li>
+			`).join("")
+		}
+		</ul>
+        `;
+    }
+    addState(states);
+
+    const [location, leftState, rightState] = states;
+    const state = location === LEFT_POSITION ? leftState : rightState;
+
+    const action_result = reflex_agent(location, state);
+
+    document.getElementById("log").innerHTML += `
+    <br>State: A: ${leftState} | B: ${rightState}
+    <br>Location: ${location} | Action: ${action_result}<br>`;
+
+    if (action_result === CLEAN_ACTION) {
+        if (location === LEFT_POSITION) states[1] = CLEAN_STATE;
+        else if (location === RIGHT_POSITION) states[2] = CLEAN_STATE;
+    }
+
+    if (action_result === MESS_ACTION) {
+        if (location === LEFT_POSITION) states[1] = DIRTY_STATE;
+        else if (location === RIGHT_POSITION) states[2] = DIRTY_STATE;
+    }
+
+    else if (action_result === RIGHT_ACTION) states[0] = RIGHT_POSITION;
+    else if (action_result === LEFT_ACTION) states[0] = LEFT_POSITION;
+    setTimeout(function () { test(states); }, 200);
+}
+
+const states = [LEFT_POSITION, DIRTY_STATE, DIRTY_STATE];
 test(states);
